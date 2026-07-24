03c6b32a87714bcdaf1acb4a14df7f8924af967a
renzomendoza-dev <renzomendoza.dev@gmail.com>
2026-07-24 08:35:35 +0800

    Feat

--- app/src/main/java/com/bcconstructionservices/app/config/OpenApiConfig.java
+++ app/src/main/java/com/bcconstructionservices/app/config/OpenApiConfig.java
@@ -1,16 +1,28 @@
 package com.bcconstructionservices.app.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String API_KEY_SCHEME_NAME = "ApiKeyAuth";
+
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(API_KEY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-API-Key")))
+                .addSecurityItem(new SecurityRequirement().addList(API_KEY_SCHEME_NAME))
                 .info(new Info()
                         .title("BC Construction Services API")
                         .version("1.0.0")
